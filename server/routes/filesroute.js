f2c44ac38ed19ce72e985227b334af25d3f0e855
Pratham1128 <prathameshmadane45@gmail.com>
2026-01-04 22:04:07 +0530

    file router modified

--- server/routes/filesroute.js
+++ server/routes/filesroute.js
@@ -11,42 +11,14 @@ import { deleteFileController } from "../controllers/file/deleteFile.controller.
 import { editFileController } from "../controllers/file/editFile.controller.js";
 const router = express.Router();
 
-router.post(
-  "/upload",
-  upload.array("uploadedFiles"),
-  uploadFileController
-);
-
+router.post("/upload", upload.array("uploadedFiles"), uploadFileController);
 
 router.get("/:id", getFileController);
-
-router.delete("/:id",checkAuth,deleteFileController);
-router.patch("/test/:id",checkAuth,editFileController);
-router.patch("/rename/:id", async (req, res, next) => {
-  const { id } = req.params;
-
-  const { oldFilename, newFilename } = req?.body;
-  if (!newFilename) {
-    return res.status(404).json({ message: "Please Enter the file name " });
-  }
-  try {
-    //db call
-    
-
-    res.status(200).json({
-      status: "successror",
-      message: "File renamed sucessfully",
-    });
-  } catch (error) {
-    error.status = 510;
-    next(error);
-  }
-});
+router.delete("/:id", checkAuth, deleteFileController);
+router.patch("/:id", checkAuth, editFileController);
 
 router.param("id", validateIdMiddleware, () => {
   console.log("rename was running");
 });
-
-
 
 export default router;
